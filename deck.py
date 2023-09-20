5c487a2cf65d2bc1f1a3a0f16672329d398f926b
Jeromeg282 <113056411+Jeromeg282@users.noreply.github.com>
2023-09-19 20:43:57 -0700

    .

--- deck.py
+++ deck.py
@@ -3,3 +3,7 @@
 ranks = ['A','2','3','4','5','6','7','8','9','T','J','Q','K']
 suits = ['C','D','H','S']
 
+deck = [rank + suit for rank in ranks for suit in suits]
+print(f"All 52 cards in the deck: {deck}")
+print("First 5 cards in the deck: ")
+print(deck[:5])
